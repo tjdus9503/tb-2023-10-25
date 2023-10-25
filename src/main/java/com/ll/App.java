22582e201ef1bb5d19b0549767297149cffdf76d
tjdus9503 <tjdus03095@gmail.com>
2023-10-25 17:44:13 +0900

    스캐너 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,16 @@
 package com.ll;
 
+import java.util.Scanner;
+
 class App {
     void run() {
+        System.out.println("== 명령 앱 ==");
 
+        System.out.print("명령) ");
+
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+
+        System.out.printf("입력 받은 명령 : %s\n", cmd);
     }
 }
