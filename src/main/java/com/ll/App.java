b2b033a79cb2591e418961ca030ef06d745ba0c0
tjdus9503 <tjdus03095@gmail.com>
2023-10-25 17:48:10 +0900

    1단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,11 +6,17 @@ class App {
     void run() {
         System.out.println("== 명령 앱 ==");
 
-        System.out.print("명령) ");
+        while (true) {
+            System.out.print("명령) ");
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
 
-        System.out.printf("입력 받은 명령 : %s\n", cmd);
+            if (cmd.equals("종료")) {
+                break;
+            }
+
+            System.out.printf("입력 받은 명령어 : %s\n", cmd);
+        }
     }
 }
