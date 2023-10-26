3666a4578111cd1c42d55857b04005d93803f5c7
tjdus9503 <tjdus03095@gmail.com>
2023-10-26 13:35:23 +0900

    step 5, print list

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -34,7 +34,17 @@ class App {
 
                 System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
             } else if (cmd.equals("목록")) {
-                System.out.println("총 개수 : " + quotations.size());
+                System.out.println("번호 / 작가 / 명언");
+
+                System.out.println("--------------------------");
+
+                if (quotations.isEmpty())
+                    System.out.println("등록된 명언이 없습니다.");
+
+                for (int i = quotations.size() - 1; i >= 0; i--) {
+                    Quotation quotation = quotations.get(i);
+                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
+                }
             }
         }
     }
