ca7bc18783f33706ab8c0f78698cba7a8d442bef
tjdus9503 <tjdus03095@gmail.com>
2023-10-26 15:24:47 +0900

    step 6, query param split

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -65,8 +65,26 @@ class App {
     }
 
     void actionRemove(String cmd) {
-        String idStr = cmd.replace("삭제?id=", "");
-        int id = Integer.parseInt(idStr);
+        String[] cmdBits = cmd.split("\\?", 2);
+        String action = cmdBits[0];
+        String queryString = cmdBits[1];
+
+        String[] queryStringBits = queryString.split("&");
+
+        int id = 0;
+
+        for (int i = 0; i < queryStringBits.length; i++) {
+            String queryParamStr = queryStringBits[i];
+
+            String[] queryParamStrBits = queryParamStr.split("=", 2);
+
+            String paramName = queryParamStrBits[0];
+            String paramValue = queryParamStrBits[1];
+
+            if (paramName.equals("id")) {
+                id = Integer.parseInt(paramValue);
+            }
+        }
 
         System.out.printf("%d번 명언을 삭제합니다.\n", id);
     }
